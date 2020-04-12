880dbf255b4745358a6c28571406147e4d573a3a
Elemigrante <gasdom@yandex.ru>
2020-04-12 13:49:44 +0300

    Added actions for PostsController

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,20 @@ RSpec.feature 'Posts', type: :feature do
       expect(page).to have_text(/Posts/)
     end
   end
+  
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+    
+    it 'can be created from new form page' do
+      visit new_post_path
+      
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Anything rationale'
+      click_button 'Save'
+      expect(page).to have_text('Anything rationale')
+    end
+  end
 end
