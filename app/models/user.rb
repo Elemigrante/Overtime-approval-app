99b1a40bd49b4a9679d0105451b31b2db60d5cc3
Elemigrante <gasdom@yandex.ru>
2020-04-16 15:18:57 +0300

    Updated style for first, last user name for post view

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :first_name, :last_name, presence: true
+  
+  def full_name
+    "#{last_name.upcase}, #{first_name.upcase}"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create!(email:                 'test@test.com',
+                         password:              'asdfasdf',
+                         password_confirmation: 'asdfasdf',
+                         first_name:            'John',
+                         last_name:             'Wick')
+  end
+  
   describe 'creation' do
-    before do
-      @user = User.create!(email:                 'test@test.com',
-                          password:              'asdfasdf',
-                          password_confirmation: 'asdfasdf',
-                          first_name:            'John',
-                          last_name:             'Wick')
-    end
     
     it 'can be created' do
       expect(@user).to be_valid
@@ -16,8 +17,14 @@ RSpec.describe User, type: :model do
     
     it 'cannot be created without first_name, last_name' do
       @user.first_name = nil
-      @user.last_name = nil
+      @user.last_name  = nil
       expect(@user).to_not be_valid
+    end
+  end
+  
+  describe 'custom name methods' do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("WICK, JOHN")
     end
   end
 end
