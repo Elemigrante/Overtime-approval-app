1917531921fd27cdd6a60d52688ffe72243773fe
Elemigrante <gasdom@yandex.ru>
2020-05-04 12:14:32 +0300

    Customized search and admin dashboard

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link administrate/application.css
-//= link administrate/application.js
+// = link administrate/application.css
+// = link administrate/application.js
