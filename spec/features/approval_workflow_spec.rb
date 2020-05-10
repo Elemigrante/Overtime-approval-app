907befb70836f5e2c9a09226fd716f7c09751fcb
Elemigrante <gasdom@yandex.ru>
2020-05-10 15:53:03 +0300

    Integrated permissions to hide status form element from non admins

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -8,8 +8,19 @@ RSpec.feature 'Approval workflow' do
   
   describe "edit" do
     let!(:post) { FactoryBot.create(:post) }
+    
     before do
       visit edit_post_path(post)
+    end
+    
+    it 'can not be eddited by a non admin' do
+      logout :user
+      user = FactoryBot.create(:user)
+      login_as(user, scope: :user)
+
+      visit edit_post_path(post)
+
+      expect(page).to have_no_content("Submitted")
     end
     
     context "when it have submitted status" do
@@ -27,19 +38,19 @@ RSpec.feature 'Approval workflow' do
         expect(post.reload.rejected?).to be_truthy
       end
     end
-
+    
     context "when it have approved status" do
       before do
         post.update(status: "approved")
       end
-  
+      
       it "sets the post to a submitted status" do
         choose "post_status_submitted"
         click_button "Save"
         
         expect(post.reload.submitted?).to be_truthy
       end
-  
+      
       it "sets the post to a rejected status" do
         choose "post_status_rejected"
         click_button "Save"
@@ -47,19 +58,19 @@ RSpec.feature 'Approval workflow' do
         expect(post.reload.rejected?).to be_truthy
       end
     end
-
+    
     context "when it have rejected status" do
       before do
         post.update(status: "rejected")
       end
-  
+      
       it "sets the post to a submitted status" do
         choose "post_status_submitted"
         click_button "Save"
         
         expect(post.reload.submitted?).to be_truthy
       end
-  
+      
       it "sets the post to a rejected status" do
         choose "post_status_approved"
         click_button "Save"
@@ -67,6 +78,6 @@ RSpec.feature 'Approval workflow' do
         expect(post.reload.approved?).to be_truthy
       end
     end
-    
+  
   end
 end
