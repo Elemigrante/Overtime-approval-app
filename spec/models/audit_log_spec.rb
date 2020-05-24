2a3413d0bede79794b044197c5bf573cc1262bd5
Elemigrante <gasdom@yandex.ru>
2020-05-24 17:16:05 +0300

    Integrated validations and defaults for audit log attributes

--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:audit_log) { FactoryBot.create(:audit_log, user: user) }
+  
+  describe 'creation' do
+    it 'can be created' do
+      expect(audit_log).to be_valid
+    end
+  end
+  
+  describe 'validations' do
+    it 'has a user associated' do
+      audit_log.user_id = nil
+      expect(audit_log).to_not be_valid
+    end
+    
+    it 'has a status' do
+      audit_log.status = nil
+      expect(audit_log).to_not be_valid
+    end
+    
+    it 'has a start_date' do
+      audit_log.start_date = nil
+      expect(audit_log).to_not be_valid
+    end
+    
+    it 'has a start_date of 6 days ago' do
+      expect(audit_log.start_date).to eq(Date.today - 6.days)
+    end
+  end
 end
--- spec/requests/audit_logs_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "AuditLogs", type: :request do
-
-end
