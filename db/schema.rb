2140febab3e603d91fe5bd355ec273b7a80c4d04
Elemigrante <gasdom@yandex.ru>
2020-05-24 15:14:44 +0300

    Created resource for Audit log with model relationship connections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_085933) do
+ActiveRecord::Schema.define(version: 2020_05_24_120417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2020_05_23_085933) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "audit_logs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "status", default: 0
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_audit_logs_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -61,5 +71,6 @@ ActiveRecord::Schema.define(version: 2020_05_23_085933) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "audit_logs", "users"
   add_foreign_key "posts", "users"
 end
