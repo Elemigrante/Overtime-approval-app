025178e0c9f0d6348ed46138143272ff322a9d8e
Elemigrante <gasdom@yandex.ru>
2020-05-25 15:35:01 +0300

    Integrated application helper for status labels and integrated enums for audit log

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,38 @@ module ApplicationHelper
   def active?(path)
     "active" if current_page?(path)
   end
-
-  def custom_bootstrap_flash
-    flash_messages = []
-    flash.each do |type, message|
-      type = 'info'
-      type = 'success' if type == 'notice'
-      type = 'error'   if type == 'alert'
-      text = "<script>toastr.#{type}('#{message}');</script>"
-      flash_messages << text.html_safe if message
-    end
-    flash_messages.join("\n").html_safe
+  
+  def status_badge status
+    status_span_generator status
   end
+  
+  private
+  
+  def status_span_generator status
+    case status
+      when 'submitted'
+        content_tag(:span, status.titleize, class: 'badge badge-primary')
+      when 'approved'
+        content_tag(:span, status.titleize, class: 'badge badge-success')
+      when 'rejected'
+        content_tag(:span, status.titleize, class: 'badge badge-danger')
+      when 'pending'
+        content_tag(:span, status.titleize, class: 'badge badge-info')
+      when 'confirmed'
+        content_tag(:span, status.titleize, class: 'badge badge-success')
+    end
+  end
+    
+    # Bootstrap default allert helper
+    # def custom_bootstrap_flash
+    #   flash_messages = []
+    #   flash.each do |type, message|
+    #     type = 'info'
+    #     type = 'success' if type == 'notice'
+    #     type = 'error'   if type == 'alert'
+    #     text = "<script>toastr.#{type}('#{message}');</script>"
+    #     flash_messages << text.html_safe if message
+    #   end
+    #   flash_messages.join("\n").html_safe
+    # end
 end
--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,12 +1,14 @@
 class AuditLog < ApplicationRecord
+  enum status: { pending: 0, confirmed: 1 }
+  
   belongs_to :user
   
   validates :user_id, :status, :start_date, presence: true
   
   after_initialize :set_defaults
-
+  
   private
-
+  
   def set_defaults
     self.start_date ||= Date.today - 6.days
   end
