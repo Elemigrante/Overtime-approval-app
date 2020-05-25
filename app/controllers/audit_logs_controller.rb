fbd75ea34f8f934c50e06dd4a21bf2f5f1963ec3
Elemigrante <gasdom@yandex.ru>
2020-05-25 15:05:16 +0300

    Blocked non admin users from accessing index page for audits

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,5 +1,6 @@
 class AuditLogsController < ApplicationController
   def index
     @audit_log = AuditLog.all
+    authorize @audit_log
   end
 end
--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature 'AuditLog', type: :feature do
   let!(:audit_log) { FactoryBot.create(:audit_log) }
   let(:admin_user) { FactoryBot.create(:admin_user) }
+  let(:user) { FactoryBot.create(:user) }
   
   before do
     login_as(admin_user, scope: :user)
@@ -19,8 +20,12 @@ RSpec.feature 'AuditLog', type: :feature do
       expect(page).to have_text(/WICK/)
     end
     
-    xit 'cannot be accessed by non admin user' do
-    
+    it 'cannot be accessed by non admin user' do
+      logout(:user)
+      login_as(user, scope: :user)
+      
+      visit audit_logs_path
+      expect(current_path).to eq(root_path)
     end
   end
 end
