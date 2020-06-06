726f65302d53ab45c661b14e1c162ca141e5dec0
Elemigrante <gasdom@yandex.ru>
2020-06-06 14:53:25 +0300

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,29 +18,31 @@ AdminUser.create!(email:                 'admin@user.com',
 
 puts '1 Admin User created'
 
-AuditLog.create(
-  user_id:    @user.id,
-  status:     0,
-  start_date: Date.today - 6.days
+AuditLog.create(user_id:    @user.id,
+                status:     0,
+                start_date: Date.today - 6.days
 )
 
-AuditLog.create(
-  user_id:    @user.id,
-  status:     0,
-  start_date: Date.today - 13.days
+AuditLog.create(user_id:    @user.id,
+                status:     0,
+                start_date: Date.today - 13.days
 )
 
-AuditLog.create(
-  user_id:    @user.id,
-  status:     0,
-  start_date: Date.today - 20.days
+AuditLog.create(user_id:    @user.id,
+                status:     0,
+                start_date: Date.today - 20.days
 )
 
 puts '3 AuditLogs have been created'
 
 100.times do |post|
   Post.create(date:             Date.today,
-              rationale:        Faker::Lorem.paragraph,
+              rationale:        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet ex egestas,
+                                 ultricies augue in, sollicitudin quam. Donec suscipit feugiat iaculis. Donec vitae ante
+                                 pharetra, pulvinar eros non, euismod magna. Nullam mollis est at tellus
+                                 egestas fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+                                 per inceptos himenaeos. Ut blandit euismod nulla at sollicitudin. Donec pharetra odio
+                                 et tortor venenatis porta.',
               user_id:          @user.id,
               overtime_request: 2.5
   )
